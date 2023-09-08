a4584a48b49bf12c6cb22219c452de0510991067
Victus <Victus@DESKTOP-HI2FRS7>
2023-09-08 18:38:15 +0400

    Add project files.

---
+++ Delegate/MyDelegate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate void MyDelegate();
+    public delegate string MyDelegateString(string s);
+}
